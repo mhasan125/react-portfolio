4c484259c4f1a34c9feb265a9cacca0399b67782
mhasan125 <hasan.prince1234567@gmail.com>
2024-09-12 08:31:09 +0500

    commit name

---
+++ src/components/Contact.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+// import { FaInstagram } from "react-icons/fa";
+import { CiFacebook } from "react-icons/ci";
+import { CiLinkedin } from "react-icons/ci";
+// import { FaSquareXTwitter } from "react-icons/fa6";
+import { FaGithubSquare } from "react-icons/fa";
+import { SiGmail } from "react-icons/si";
+
+
+const Contact = () => {
+  return (
+    <>
+      <div className="container contact" id="contact">
+        <h1>CONTACT ME</h1>
+        <div
+          className="contact-icon"
+          data-aos="zoom-in-up"
+          data-aos-duration="1000"
+        >
+          {/* <a href="https://www.google.com" target="_blank" className="items">
+            <FaInstagram className="icons" />
+          </a> */}
+          <a href="https://www.facebook.com/ahsan.prince.7146" target="_blank" className="items">
+            <CiFacebook className="icons" />
+          </a>
+          <a href="https://www.linkedin.com/in/ahsan-javed-95264a2b9/" target="_blank" className="items">
+            <CiLinkedin className="icons" />
+          </a>
+          {/* <a href="https://www.google.com" target="_blank" className="items">
+            <FaSquareXTwitter className="icons" />
+          </a> */}
+          <a href="https://github.com/mhasan125" target="_blank" className="items">
+            <FaGithubSquare className="icons" />
+          </a>
+          <a
+            href="https://hasan.prince1234567@gmail.com"
+            target="_blank"
+            className="items"
+          >
+            <SiGmail className="icons" />
+          </a>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Contact;
---
+++ src/components/Experience.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import experience from "./data/experience.json";
+import { BiColor } from "react-icons/bi";
+
+const Experience = () => {
+  return (
+    <>
+    { <div className="container ex" id="experience"> 
+    <h1>EXPERIENCE</h1>
+    <h1>SMIT </h1>
+    <h3 style={ {color: "yellow"}}>I am work SMIT in various Project with experties I'm  a highly motivated Full Stack Developer with a keen eye for detail and a passion for crafting high-quality web applications. My journey in the tech world began with HTML and CSS, and I've since evolved into a versatile developer proficient in the latest technologies.</h3>
+     
+      </div>}
+    </>
+  );
+};
+
+export default Experience;
